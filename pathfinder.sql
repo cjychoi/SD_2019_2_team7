12293a4f4729e3e52e5db67ab2dc92ff7a0e16e2
Beomjun-Kim <45474966+Beomjun-Kim@users.noreply.github.com>
2019-12-17 21:56:23 +0900

    add BUS and METRO

--- pathfinder.sql
+++ pathfinder.sql
@@ -162,6 +162,135 @@ INSERT INTO RESTAURANT VALUES ('앙뚜아네트','용두암','3.9');
 INSERT INTO RESTAURANT VALUES ('돈대장','용두암','3.3');
 INSERT INTO RESTAURANT VALUES ('용두암오메기떡','용두암','4.3');
 
+INSERT INTO METRO VALUES ('경복궁','3','경복궁역');
+INSERT INTO BUS VALUES ('경복궁','109','경복궁');
+INSERT INTO BUS VALUES ('경복궁','171','경복궁');
+INSERT INTO BUS VALUES ('경복궁','272','경복궁');
+INSERT INTO BUS VALUES ('경복궁','601','경복궁');
+INSERT INTO BUS VALUES ('경복궁','606','경복궁');
+INSERT INTO BUS VALUES ('경복궁','710','경복궁');
+INSERT INTO BUS VALUES ('경복궁','1020','경복궁');
+
+INSERT INTO BUS VALUES ('남산 타워','순환 TOUR1','남산서울타워');
+INSERT INTO BUS VALUES ('남산 타워','순환 TOUR4','남산서울타워');
+INSERT INTO BUS VALUES ('남산 타워','순환 02','남산서울타워');
+INSERT INTO BUS VALUES ('남산 타워','순환 03','남산서울타워');
+INSERT INTO BUS VALUES ('남산 타워','순환 05','남산서울타워');
+
+INSERT INTO METRO VALUES ('반포 한강 공원','9','신반포역');
+INSERT INTO BUS VALUES ('반포 한강 공원','143','반포대교남단 한강시민공원입구');
+INSERT INTO BUS VALUES ('반포 한강 공원','401','반포대교남단 한강시민공원입구');
+INSERT INTO BUS VALUES ('반포 한강 공원','406','반포대교남단 한강시민공원입구');
+INSERT INTO BUS VALUES ('반포 한강 공원','740','반포한강공원.세빛섬');
+
+INSERT INTO METRO VALUES ('롯데 월드','2','잠실역');
+INSERT INTO BUS VALUES ('롯데 월드','301','잠실역.롯데월드');
+INSERT INTO BUS VALUES ('롯데 월드','341','잠실역.롯데월드');
+INSERT INTO BUS VALUES ('롯데 월드','342','잠실역.롯데월드');
+INSERT INTO BUS VALUES ('롯데 월드','360','잠실역.롯데월드');
+INSERT INTO BUS VALUES ('롯데 월드','362','잠실역.롯데월드');
+
+INSERT INTO METRO VALUES ('통일 시장','4','동대문역');
+INSERT INTO BUS VALUES ('통일 시장','105','동대문역사문화공원');
+INSERT INTO BUS VALUES ('통일 시장','144','동대문역사문화공원');
+INSERT INTO BUS VALUES ('통일 시장','261','동대문역사문화공원');
+INSERT INTO BUS VALUES ('통일 시장','301','동대문역사문화공원');
+INSERT INTO BUS VALUES ('통일 시장','420','동대문역사문화공원');
+
+INSERT INTO METRO VALUES ('광안리 해수욕장','2','광안역');
+INSERT INTO BUS VALUES ('광안리 해수욕장','41','광안리해수욕장');
+INSERT INTO BUS VALUES ('광안리 해수욕장','42','광안리해수욕장');
+
+INSERT INTO METRO VALUES ('자갈치 시장','1','자갈치역');
+INSERT INTO METRO VALUES ('자갈치 시장','1','남포역');
+INSERT INTO BUS VALUES ('자갈치 시장','113','자갈치시장');
+INSERT INTO BUS VALUES ('자갈치 시장','30','자갈치시장');
+INSERT INTO BUS VALUES ('자갈치 시장','6','자갈치시장');
+INSERT INTO BUS VALUES ('자갈치 시장','7','자갈치시장');
+INSERT INTO BUS VALUES ('자갈치 시장','71','자갈치시장');
+
+INSERT INTO BUS VALUES ('감천 문화 마을','사하구1','감정초등학교.감천문화마을');
+INSERT INTO BUS VALUES ('감천 문화 마을','사하구1-1','감정초등학교.감천문화마을');
+INSERT INTO BUS VALUES ('감천 문화 마을','서구2','감정초등학교.감천문화마을');
+INSERT INTO BUS VALUES ('감천 문화 마을','서구2-2','감정초등학교.감천문화마을');
+
+INSERT INTO METRO VALUES ('해운대 해수욕장','2','해운대역');
+INSERT INTO BUS VALUES ('해운대 해수욕장','139','해운대해수욕장');
+INSERT INTO BUS VALUES ('해운대 해수욕장','307','해운대해수욕장');
+INSERT INTO BUS VALUES ('해운대 해수욕장','1003','해운대해수욕장');
+
+INSERT INTO METRO VALUES ('동백섬','2','동백역');
+INSERT INTO BUS VALUES ('동백섬','139','동백섬입구');
+INSERT INTO BUS VALUES ('동백섬','307','동백섬입구');
+INSERT INTO BUS VALUES ('동백섬','1003','동백섬입구');
+
+INSERT INTO METRO VALUES ('이월드','2','두류역');
+INSERT INTO BUS VALUES ('이월드','653','두류파출소앞');
+INSERT INTO BUS VALUES ('이월드','달서 4-1','이월드후문건너');
+INSERT INTO BUS VALUES ('이월드','서구 1-1','이월드후문건너');
+INSERT INTO BUS VALUES ('이월드','달서4','이월드 후문앞');
+INSERT INTO BUS VALUES ('이월드','서구1','이월드 후문앞');
+
+INSERT INTO METRO VALUES ('김광석 거리','2','경대병원역');
+INSERT INTO BUS VALUES ('김광석 거리','309','방청시장앞');
+INSERT INTO BUS VALUES ('김광석 거리','403','방청시장앞');
+INSERT INTO BUS VALUES ('김광석 거리','609','방청시장앞');
+INSERT INTO BUS VALUES ('김광석 거리','649','방청시장앞');
+INSERT INTO BUS VALUES ('김광석 거리','840','방청시장앞');
+
+INSERT INTO BUS VALUES ('앞산 전망대','410-1','앞산공원관리사무소건너');
+INSERT INTO BUS VALUES ('앞산 전망대','남구1','앞산공원관리사무소건너');
+INSERT INTO BUS VALUES ('앞산 전망대','남구1-1','앞산공원관리사무소건너');
+INSERT INTO BUS VALUES ('앞산 전망대','410','앞산공원관리사무소 앞');
+INSERT INTO BUS VALUES ('앞산 전망대','300','앞산공원');
+
+INSERT INTO METRO VALUES ('수성못','3','수성못역');
+INSERT INTO BUS VALUES ('수성못','410-1','수성못건너');
+INSERT INTO BUS VALUES ('수성못','410','수성못앞');
+INSERT INTO BUS VALUES ('수성못','401','수성못앞');
+INSERT INTO BUS VALUES ('수성못','449','수성랜드앞');
+INSERT INTO BUS VALUES ('수성못','403','두산행정복지센터앞');
+
+INSERT INTO BUS VALUES ('성산일출봉','201','성산일출봉입구');
+INSERT INTO BUS VALUES ('성산일출봉','211','성산일출봉입구');
+INSERT INTO BUS VALUES ('성산일출봉','213','성산일출봉입구');
+INSERT INTO BUS VALUES ('성산일출봉','113','성산일출봉입구');
+
+INSERT INTO BUS VALUES ('용두암','432','제주사대부설중학교');
+INSERT INTO BUS VALUES ('용두암','431','제주사대부설중학교');
+INSERT INTO BUS VALUES ('용두암','453','제주사대부설중학교');
+INSERT INTO BUS VALUES ('용두암','434','제주사대부설중학교');
+
+INSERT INTO BUS VALUES ('만장굴','711-1','만장굴');
+INSERT INTO BUS VALUES ('만장굴','711-2','만장굴');
+
+INSERT INTO BUS VALUES ('첨성대','60','첨성대');
+INSERT INTO BUS VALUES ('첨성대','61','첨성대');
+INSERT INTO BUS VALUES ('첨성대','609','월성동주민센타');
+INSERT INTO BUS VALUES ('첨성대','10','첨성대');
+INSERT INTO BUS VALUES ('첨성대','11','첨성대');
+
+INSERT INTO BUS VALUES ('불국사','10','불국사');
+INSERT INTO BUS VALUES ('불국사','700','불국사');
+INSERT INTO BUS VALUES ('불국사','12','불국사');
+INSERT INTO BUS VALUES ('불국사','12-1','불국사');
+INSERT INTO BUS VALUES ('불국사','11','불국사');
+
+INSERT INTO BUS VALUES ('보문 단지','100-1','경상북도문화관광공사');
+INSERT INTO BUS VALUES ('보문 단지','10','경상북도문화관광공사');
+INSERT INTO BUS VALUES ('보문 단지','16','경상북도문화관광공사');
+INSERT INTO BUS VALUES ('보문 단지','700','경상북도문화관광공사');
+
+INSERT INTO BUS VALUES ('경주 월드','100-1','경주월드.캘리포니아비치');
+INSERT INTO BUS VALUES ('경주 월드','10','경주월드.캘리포니아비치');
+INSERT INTO BUS VALUES ('경주 월드','16','경주월드.캘리포니아비치');
+INSERT INTO BUS VALUES ('경주 월드','11','경주월드.캘리포니아비치');
+INSERT INTO BUS VALUES ('경주 월드','700','경주월드.캘리포니아비치');
+
+INSERT INTO BUS VALUES ('추억의 달동네','10','민속공예촌.신라역사과학관');
+INSERT INTO BUS VALUES ('추억의 달동네','11','민속공예촌.신라역사과학관');
+INSERT INTO BUS VALUES ('추억의 달동네','700','민속공예촌.신라역사과학관');
+
 
 
 ALTER TABLE RESTAURANT ADD FOREIGN KEY (Dname) REFERENCES TOURIST_DESTINATION (Dname) ON DELETE CASCADE ON UPDATE CASCADE;
